27cc7747c0123a825c4a2e5323224e22c77f7642
Giildo <giildo.jm@gmail.com>
2020-09-13 09:10:37 +0200

    💄 Create the navbar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
+  <v-app>
+    <nav-bar/>
     <router-view/>
-  </div>
+  </v-app>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+html {
+  font-size: 20px;
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  #app {
+    font-family: Roboto, sans-serif;
   }
 }
 </style>
+<script>
+import NavBar from '@/components/template/NavBar'
+export default {
+  components: { NavBar },
+}
+</script>
---
+++ src/components/template/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <router-link :to="{ name: 'Home' }">
+      <img :src="require('../../assets/img/logo.png')" alt="Logo du site représentant le Choixpeau de Harry potter."/>
+      Quiz du Choixpeau
+    </router-link>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+}
+</script>
+
+<style lang="scss" scoped>
+@import "src/assets/style/style";
+
+header {
+  a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: $navbar;
+    font-weight: bold;
+    font-size: 3rem;
+    color: white;
+    text-decoration: none;
+  }
+}
+</style>
